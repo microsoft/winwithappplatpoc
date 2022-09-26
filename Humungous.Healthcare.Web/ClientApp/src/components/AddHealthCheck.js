faf7ae0418c1d224d53b3e59289a9b7e2027c5b8
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-09-26 09:34:58 -0500

    Creating .deployment file to point app service at correct project

---
+++ Humungous.Healthcare.Web/ClientApp/src/components/AddHealthCheck.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export class AddHealthCheck extends Component {
+    static displayName = AddHealthCheck.name
+
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    }
+
+    async createHealthCheck() {
+        
+    }
+}
