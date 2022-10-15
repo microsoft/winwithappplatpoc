15419905ef685d0b5cc2c53cab63b56fb23a9c88
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2022-10-15 13:22:46 -0500

    Updates to React web client and API CORS for local testing

--- Humongous.Healthcare.Web/ClientApp/src/components/AddHealthCheck.js
+++ Humongous.Healthcare.Web/ClientApp/src/components/AddHealthCheck.js
@@ -11,7 +11,8 @@ export class AddHealthCheck extends Component {
             patientid: '',
             date: '',
             healthstatus: '',
-            symptoms: []
+            symptoms: [],
+            symptomToAdd: ''
         };
     }
 
@@ -32,6 +33,24 @@ export class AddHealthCheck extends Component {
         console.log("Test");
         this.createHealthCheck();
     }
+    
+    handleAddSymptom = () => {
+        this.setState({ symptoms: [...this.state.symptoms, ""] });
+    }
+    
+    handleSymptomInput = (index, event) => {
+        const symptoms = [...this.state.symptoms];
+        symptoms[index] = event.target.value;
+        this.setState({ symptoms });
+    }
+    
+    handleDeleteSymptom = (index) => {
+        return () => {
+            const symptoms = this.state.symptoms;
+            symptoms.splice(index, 1);
+            this.setState({ symptoms });
+        }
+    }
 
     render() {
         return (
@@ -39,9 +58,50 @@ export class AddHealthCheck extends Component {
                 <h4>Submit a HealthCheck:</h4>
                 <form onSubmit={this.handleSubmit}>
                     <input type="hidden" name="id" placeholder="Id" value="id" />
-                    <input type="text" name="patientid" placeholder="Patient Id" value={this.state.patientid} onChange={this.handleInput} />
-                    <input type="text" name="date" placeholder="Date" value={this.state.date} onChange={this.handleInput} />
-                    <input type="text" name="healthstatus" placeholder="Health Status" value={this.state.healthstatus} onChange={this.handleInput} />
+                    <div>
+                        <label>
+                            Patient ID:
+                            <input type="text" name="patientid" placeholder="Patient Id" value={this.state.patientid} onChange={this.handleInput} />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Date:
+                            <input type="date" name="date" placeholder="Date" value={this.state.date} onChange={this.handleInput} />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Health Status:
+                            <input type="text" name="healthstatus" placeholder="Health Status" onChange={this.handleInput} />
+                        </label>
+                    </div>
+                    <div>
+                        <table className='table table-striped' aria-labelledby="tabelLabel">
+                            <thead>
+                                <tr>
+                                    <th>Symptom</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.symptoms.map((symptom, index) =>
+                                    <tr key={index}>
+                                        <td>
+                                            <input type="text" name="symptom" placeholder="Symptom" value={symptom || ""} onChange={e => this.handleSymptomInput(index, e)} />
+                                        </td>
+                                        <td><button className="button delete" type="button" onClick={this.handleDeleteSymptom(index)}>Delete</button></td>
+                                    </tr>
+                                )}
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>
+                                        <button className="button add" type="button" onClick={this.handleAddSymptom}>Add Symptom</button>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
                     <button type="submit">Submit</button>
                 </form>
             </div>
